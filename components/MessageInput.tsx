d615d47a9697c29c45d70d6ffa303fe1e4b46b3c
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2024-07-09 18:40:47 +0530

    ✨ Add shadows to the Messge input component

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -11,9 +11,10 @@ const MessageInput = (props: Props) => {
             <Input
                 type="text"
                 placeholder="Message LocalGPT"
-                className="max-w-screen-sm p-6 focus-visible:ring-0 focus-visible:ring-offset-0 shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+                className="max-w-screen-sm p-6 focus-visible:ring-0 focus-visible:ring-offset-0  shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:shadow-[0_3px_10px_rgb(255,255,255,0.1)]"
             />
-            <Button className="ml-3 py-6">
+
+            <Button className="ml-3 py-6  shadow-[0_3px_10px_rgb(0,0,0,0.4)] dark:shadow-[0_3px_10px_rgb(255,255,255,0.4)]">
                 <Send />
             </Button>
         </div>
